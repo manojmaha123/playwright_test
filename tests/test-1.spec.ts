0a1df2585ea07c909ed8883dd4a2de025b7886a0
manojmaha123 <manojmahanandi123@gmail.com>
2025-03-10 23:06:12 +0530

    Update test-1.spec.ts

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -7,38 +7,18 @@ const authToken = '6edc656d60c1a7337c4c8ec16f17d578';
 const client = twilio(accountSid, authToken);
 
 test('test', async ({ page, context }) => {
-  await page.goto('https://paytm.com/movies/bengaluru');
-  const [newPage] = await Promise.all([
-    context.waitForEvent('page'),
-    page.locator('.PageBlockWidget_closeBtn__MdHIU').click()
-  ]);
-  await newPage.close();
-  await page.getByRole('radio', { name: 'Telugu filter' }).check();
-  const movieLink = await page.getByRole('link', { name: 'Seethamma Vakitlo Sirimalle Chettu (2013) Seethamma Vakitlo Sirimalle Chettu (' });
-  if (await movieLink.isVisible()) {
-    await movieLink.click();
-    await page.getByRole('link', { name: 'Fri 7' }).click();
-    await page.getByRole('link', { name: 'Sandhya Theatre RGB Laser' }).click();
-
-
-    client.messages.create({
-      body: 'Seethamma Vakitlo Sirimalle Chettu is now available at Sandhya Theatre RGB Laser on Friday 7th.',
+  await page.goto('https://shop.royalchallengers.com/ticket');
+  await page.waitForLoadState('networkidle');
+  const buyTicketsButtons = await page.locator('button', { hasText: 'BUY TICKETS' }).count();
+  
+  if (buyTicketsButtons === 2) {
+    client.calls.create({
+      twiml: '<Response><Say>RCB tickets are now available. Hurry up and book your tickets now.</Say></Response>',
       from: '+15075854277',
       to: '+916309649279'
-    }).then(message => console.log(`Message sent: ${message.sid}`))
-      .catch(error => console.error(`Failed to send message: ${error}`));
+    }).then(call => console.log(`Call initiated: ${call.sid}`))
+      .catch(error => console.error(`Failed to initiate call: ${error}`));
   } else {
-    console.log('Movie not found.');
+    console.log(`Tickets are not available.`);
   }
-  
-    // Send call notification
-  //   client.calls.create({
-  //     twiml: '<Response><Say>Seethamma Vakitlo Sirimalle Chettu is now available at Sandhya Theatre RGB Laser on Friday 7th.</Say></Response>',
-  //     to: '+916309649279',
-  //     from: '+15075854277'
-  //   }).then(call => console.log(`Call initiated: ${call.sid}`))
-  //     .catch(error => console.error(`Failed to initiate call: ${error}`));
-  // } else {
-  //   console.log('Movie not found.');
-  // }
 });
